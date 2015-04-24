c715902572801f2657b6133fe74252190affd146
peterneubauer <peter@neubauer.se>
2015-04-24 14:20:33 +0200

    adjusting gem home

--- templates/riemann_health_run.sh
+++ templates/riemann_health_run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.3/bin/
+export PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.3/bin/
+export GEM_HOME=/usr/local/rvm/gems/ruby-2.1.3
 {% if rieman_extra_tags is defined %}
 exec riemann-health --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-health {{ rieman_extra_tags }}
 {% else %}
