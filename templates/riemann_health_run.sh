8d7c8f9697686713c8ed00ea86a7a20f0b37eacc
peterneubauer <peter@neubauer.se>
2015-09-08 16:08:09 +0200

    bigger interval

--- templates/riemann_health_run.sh
+++ templates/riemann_health_run.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.3/bin/
 export GEM_HOME=/usr/local/rvm/gems/ruby-2.1.3
 {% if rieman_extra_tags is defined %}
-exec riemann-health --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-health {{ rieman_extra_tags }}
+exec riemann-health --interval=20 --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-health {{ rieman_extra_tags }}
 {% else %}
-exec riemann-health --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-health
+exec riemann-health --interval=20 --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-health
 {% endif %}
--- templates/riemann_net_run.sh
+++ templates/riemann_net_run.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.3/bin/
 export GEM_HOME=/usr/local/rvm/gems/ruby-2.1.3
 {% if rieman_extra_tags is defined %}
-exec riemann-net --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-net {{ rieman_extra_tags }}
+exec riemann-net --interval=20  --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-net {{ rieman_extra_tags }}
 {% else %}
-exec riemann-net --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-net
+exec riemann-net --interval=20 --host={{ riemann_server }} --port={{ riemann_port }} --tag=riemann-net
 {% endif %}
